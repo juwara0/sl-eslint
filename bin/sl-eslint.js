f78a03015f04a81cfba657ddadffb3a935a59c1f
Joshua Forisha <jforisha@softlayer.com>
2015-09-29 11:12:11 -0500

    v0.3.0-rc.2

--- bin/sl-eslint.js
+++ bin/sl-eslint.js
@@ -4,10 +4,29 @@ var CLIEngine = require( 'eslint' ).CLIEngine;
 var fs = require( 'fs' );
 var path = require( 'path' );
 
-var cli = new CLIEngine( fs.readFileSync( path.resolve( process.cwd(), 'node_modules', 'sl-eslint', '.eslintrc' ) ) );
-var report = cli.executeOnFiles( process.argv.slice( 2 ) );
-var formatter = cli.getFormatter();
+function exists( filePath ) {
+    try {
+        fs.statSync( filePath );
+    } catch ( error ) {
+        return false;
+    }
 
-console.log( formatter( report.results ) );
+    return filePath;
+}
 
-process.exit( report.errorCount > 0 ? 1 : 0 );
+var lintConfig;
+if ( lintConfig = (
+    exists( path.resolve( process.cwd(), 'node_modules', 'sl-eslint', '.eslintrc' ) ) ||
+    exists( path.resolve( process.cwd(), '.eslintrc' ) )
+)) {
+    var cli = new CLIEngine( fs.readFileSync( lintConfig ) );
+    var report = cli.executeOnFiles( process.argv.slice( 2 ) );
+    var formatter = cli.getFormatter();
+
+    console.log( formatter( report.results ) );
+
+    process.exit( report.errorCount > 0 ? 1 : 0 );
+} else {
+    console.error( 'No valid .eslintrc found' );
+    process.exit( 1 );
+}
