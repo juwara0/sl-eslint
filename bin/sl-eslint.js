ebd27df02f98d49943b489511fe02d7992f4bfc8
jecorley <jecorley@softlayer.com>
2015-09-29 09:40:28 -0500

    updated the code to hopefully pass its own linting; I'd test that but it would have to work on Windows so that I could test it on Windows

--- bin/sl-eslint.js
+++ bin/sl-eslint.js
@@ -2,42 +2,40 @@
 
 var packageName = "sl-eslint";
 
-var path = require('path');
-var child_process = require('child_process');
+var path = require( 'path' );
+var child_process = require( 'child_process' );
 
-var args = process.argv.slice(2);
+var args = process.argv.slice( 2 );
 
 // this is where it was called from and not a good way of doing it (but no worse than before)
 var root = process.cwd();
 
 // lint configuration file
-var lintConfig = path.resolve(root, 'node_modules', packageName, '.eslintrc');
+var lintConfig = path.resolve( root, 'node_modules', packageName, '.eslintrc' );
 // lint rules directory
-var lintRulesDir = path.resolve(root, 'node_modules', packageName, 'lib', 'rules');
+var lintRulesDir = path.resolve( root, 'node_modules', packageName, 'lib', 'rules' );
 
-var eslintPath = path.resolve('node_modules', 'sl-eslint', 'node_modules', '.bin', 'eslint');
+var eslintPath = path.resolve( 'node_modules', 'sl-eslint', 'node_modules', '.bin', 'eslint' );
+var execPath = eslintPath + " --config " + lintConfig + " --rulesdir " + lintRulesDir + " " + args.join( " " );
 
-var execPath = eslintPath + " --config " + lintConfig + " --rulesdir " + lintRulesDir + " " + args.join(" ");
-console.log( execPath );
+var cp = child_process.exec( execPath );
+cp.stdout.pipe( process.stdout );
+cp.stderr.pipe( process.stderr );
+process.stdin.pipe( cp.stdin );
 
-var cp = child_process.exec(execPath);
-cp.stdout.pipe(process.stdout);
-cp.stderr.pipe(process.stderr);
-process.stdin.pipe(cp.stdin);
-
-cp.on('error', function (err) {
-  console.error('Error executing eslint at', eslintPath);
-  console.error(err.stack);
+cp.on( 'error', function catchError( err ) {
+  console.error( 'Error executing eslint at', eslintPath );
+  console.error( err.stack );
 });
 
-cp.on('exit', function(code){
+cp.on( 'exit', function catchExit( code ) {
   // Wait few ms for error to be printed.
   setTimeout( function() {
-    process.exit(code);
-  }, 20);
+    process.exit( code );
+  }, 20 );
 });
 
-process.on('SIGTERM', function() {
-  cp.kill('SIGTERM');
-  process.exit(1);
+process.on( 'SIGTERM', function catchSigterm() {
+  cp.kill( 'SIGTERM' );
+  process.exit( 1 );
 });
